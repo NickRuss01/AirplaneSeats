d9fc7fb8390e6a2a872a7ef1804900a23a53cbbe
LosSantosSavior <nickrussell001@gmail.com>
2025-03-16 10:33:22 -0500

    created constants for array size

---
+++ src/AirplaneSeats.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class AirplaneSeats {
+    static final int x = 5;
+    static final int y = 4;
+    static Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+    }
+}
