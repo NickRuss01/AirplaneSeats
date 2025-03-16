0ee96b0289c6c95a1ce6ea0bb90435bab1187da4
LosSantosSavior <nickrussell001@gmail.com>
2025-03-16 18:44:16 -0500

    getters added

--- src/Seat.java
+++ src/Seat.java
@@ -21,5 +21,31 @@ public class Seat {
         Random rng = new Random();
         row = rowPassed;
         seat = seatPassed;
+        aisle = seatPassed == 1 || seatPassed == 2;
+
+        window = seatPassed == 0 || seatPassed == 3;
+
+        occupied = false;
+        cost = rng.nextInt(301) + 200;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public boolean getAisle() {
+        return aisle;
+    }
+
+    public boolean getWindow() {
+        return window;
+    }
+
+    public boolean getOccupied() {
+        return occupied;
     }
 }
